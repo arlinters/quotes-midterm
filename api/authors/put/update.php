422cd3c5855197804efc07140f0c70d44cfa3c93
= <charlie.winters17@gmail.com>
2022-03-09 19:00:32 -0600

    add put for authors

---
+++ api/authors/put/update.php
@@ -0,0 +1,30 @@
+<?php
+
+$author = new Author($db);
+$data = json_decode(file_get_contents("php://input"), true);
+
+if(array_key_exists("author", $data) && array_key_exists("id", $data)){
+	$author->author = $data['author'];
+	$author->id = $data['id'];
+
+	try{
+		$author->update();
+		echo json_encode(
+			['id'=>$author->id, 'author' => $author->author]
+		);
+	}
+	catch(Exception $e){
+		// set generic 500 error
+		http_response_code(500);
+		echo json_encode(
+			['message' => $e->getMessage()]
+		);
+	}
+}
+else{
+	echo json_encode(
+		array('message' => 'Missing Required Parameters')
+	);
+}
+
+?>
--- models/Author.php
+++ models/Author.php
@@ -72,5 +72,30 @@
 				}
 	 		throw new Exception('Error when inserting the author, '. $this->author .' into the database.');
 		}
-	}
-?>
+
+
+		public function update(){
+			// Create Query
+			$query = 'UPDATE '. $this->table . '
+					SET author = :author
+					WHERE id = :id';
+
+			// Prepare Statement
+			$stmt = $this->conn->prepare($query);
+
+			// Clean data
+			$this->author = htmlspecialchars(strip_tags($this->author));
+			$this->id = htmlspecialchars(strip_tags($this->id));
+
+			// Bind data
+			$stmt-> bindParam(':author', $this->author);
+			$stmt-> bindParam(':id', $this->id);
+
+			// Execute query
+			if($stmt->execute()) {
+				return;
+			}
+
+			throw new Exception('Error when updating this author, '. $this->author .', in the database.');
+  }
+}
